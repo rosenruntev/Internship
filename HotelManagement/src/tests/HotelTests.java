7f6d155d9a0138503a211aadad1c2aeafa7bd74e
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-09 18:56:55 +0300

    Added new hotel tests case

--- HotelManagement/src/tests/HotelTests.java
+++ HotelManagement/src/tests/HotelTests.java
@@ -1,8 +1,11 @@
 package tests;
 
 import core.Hotel;
+import core.Manager;
+import core.Room;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HotelTests {
@@ -12,5 +15,20 @@ public class HotelTests {
 		assertThrows(IllegalArgumentException.class, () -> {
 			Hotel hotel = new Hotel(null);
 		});
+	}
+
+	@Test
+	public void hotelShouldNotHaveAvailableRoomsIfAllRoomsAreBooked() {
+		Room firstRoom = new Room(1);
+		Room secondRoom = new Room(2);
+		Hotel hotel = new Hotel("Hotel");
+		hotel.getRooms().add(firstRoom);
+		hotel.getRooms().add(secondRoom);
+		Manager manager = new Manager("Manager", hotel);
+
+		manager.bookRoom(1);
+		manager.bookRoom(2);
+
+		assertEquals(0, manager.getAvailableRooms().size());
 	}
 }
