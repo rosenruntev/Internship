b9b385dcfe5d8c9202370195031b2d2a7c1a7c9e
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-15 08:33:22 +0300

    Changed abstract commodity name to inventory number

--- HotelManagement/src/core/Commodities/AbstractCommodity.java
+++ HotelManagement/src/core/Commodities/AbstractCommodity.java
@@ -1,15 +1,19 @@
 package core.Commodities;
 
 public abstract class AbstractCommodity {
-	private String name;
+	private int inventoryNumber;
 
 	public abstract void prepare();
 
-	public String getName() {
-		return name;
+	public int getInventoryNumber() {
+		return inventoryNumber;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setInventoryNumber(int inventoryNumber) {
+		if (inventoryNumber < 0) {
+			throw new IllegalArgumentException("Inventory number cannot be negative.");
+		}
+
+		this.inventoryNumber = inventoryNumber;
 	}
 }
