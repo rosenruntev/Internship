97d578d91ac164505d94982f033b94e74c9d89be
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-15 08:34:35 +0300

    Added number of people to bed

--- HotelManagement/src/core/Commodities/Bed.java
+++ HotelManagement/src/core/Commodities/Bed.java
@@ -1,8 +1,26 @@
 package core.Commodities;
 
 public class Bed extends AbstractCommodity {
+	private int numberOfPeople;
+
+	public Bed(int numberOfPeople) {
+		this.setNumberOfPeople(numberOfPeople);
+	}
+
 	@Override
 	public void prepare() {
 		System.out.println("Bed sheets are being replaced");
+	}
+
+	public int getNumberOfPeople() {
+		return numberOfPeople;
+	}
+
+	public void setNumberOfPeople(int numberOfPeople) {
+		if (numberOfPeople < 1) {
+			throw new IllegalArgumentException("Number of people cannot be less than 1.");
+		}
+
+		this.numberOfPeople = numberOfPeople;
 	}
 }
