f080f33a614b71d9165a98cde854735ef0a3e991
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-15 11:14:06 +0300

    Removed a test case which tests that the hotel should not have available rooms if all rooms are booked

--- HotelManagement/src/tests/HotelTests.java
+++ HotelManagement/src/tests/HotelTests.java
@@ -1,11 +1,7 @@
 package tests;
 
 import core.Hotel;
-import core.Manager;
-import core.Room;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HotelTests {
@@ -15,20 +11,5 @@ public class HotelTests {
 		assertThrows(IllegalArgumentException.class, () -> {
 			Hotel hotel = new Hotel(null);
 		});
-	}
-
-	@Test
-	public void hotelShouldNotHaveAvailableRoomsIfAllRoomsAreBooked() {
-		Room firstRoom = new Room(1);
-		Room secondRoom = new Room(2);
-		Hotel hotel = new Hotel("Hotel");
-		hotel.getRooms().add(firstRoom);
-		hotel.getRooms().add(secondRoom);
-		Manager manager = new Manager("Manager", hotel);
-
-		manager.bookRoom(1);
-		manager.bookRoom(2);
-
-		assertEquals(0, manager.getAvailableRooms().size());
 	}
 }
