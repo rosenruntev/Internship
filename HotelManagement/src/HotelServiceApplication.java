3fbcb2ec31bb35e9de278d7030a1e8cc0b027551
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-15 10:13:03 +0300

    Added rooms commodies and created bookings

--- HotelManagement/src/HotelServiceApplication.java
+++ HotelManagement/src/HotelServiceApplication.java
@@ -1,29 +1,51 @@
-import core.Hotel;
-import core.Manager;
-import core.Room;
+import core.*;
+import core.Commodities.Bed;
+import core.Commodities.Shower;
+import core.Commodities.Toilet;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class HotelServiceApplication {
 	public static void main(String[] args) {
 		Manager manager = new Manager("Manager");
 
 		Room firstRoom = new Room(1);
+		firstRoom.getCommodities().add(new Bed(1,2));
+		firstRoom.getCommodities().add(new Shower(2));
+		firstRoom.getCommodities().add(new Toilet(3));
 		Room secondRoom = new Room(2);
+		secondRoom.getCommodities().add(new Bed(1,1));
+		secondRoom.getCommodities().add(new Bed(2, 1));
+		secondRoom.getCommodities().add(new Bed(3, 1));
+		secondRoom.getCommodities().add(new Shower(4));
+		secondRoom.getCommodities().add(new Toilet(5));
+		secondRoom.getCommodities().add(new Toilet(6));
+		Room thirdRoom = new Room(3);
+		thirdRoom.getCommodities().add(new Bed(1,1));
+		thirdRoom.getCommodities().add(new Bed(2, 1));
+		thirdRoom.getCommodities().add(new Shower(3));
+		thirdRoom.getCommodities().add(new Toilet(4));
 
 		Hotel hotel = new Hotel("Hotel");
 		hotel.getRooms().add(firstRoom);
 		hotel.getRooms().add(secondRoom);
+		hotel.getRooms().add(thirdRoom);
 		manager.setHotel(hotel);
-		ArrayList<Room> freeRooms = manager.getAvailableRooms();
-		if (!freeRooms.isEmpty()) {
-			freeRooms.get(0).bookTheRoom();
-		}
 
-		hotel.getRooms().stream().forEach(r -> {
-			if (r.isBooked()) {
-				r.clearRoomBooking();
+		manager.bookRoom(2, LocalDate.of(2019, 7, 20),
+			LocalDate.of(2019, 7, 25), 5, 2, "guest1", "001");
+
+		manager.bookRoom(1, LocalDate.of(2019, 7, 22),
+			LocalDate.of(2019, 7, 22), 1, 1, "guest2", "002");
+
+		manager.bookRoom(3, LocalDate.of(2019, 7, 19),
+			LocalDate.of(2019, 7, 21), 2, 2, "guest3", "003");
+
+		for(Room room : hotel.getRooms()) {
+			for (Booking booking : room.getBookings()) {
+				System.out.println(booking.getFromDate() + " " + booking.getToDate() + " " + booking.getGuestId()
+				 + " " + booking.getGuestName());
 			}
-		});
+		}
 	}
 }
