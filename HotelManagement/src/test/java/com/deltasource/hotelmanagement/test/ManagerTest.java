8f5b6a71734a08b8f479b7078ac745b23d701414
Rosen Runtev <runtev.rosen@gmail.com>
2019-07-16 11:44:35 +0300

    Fixing bugs

--- HotelManagement/src/test/java/com/deltasource/hotelmanagement/test/ManagerTest.java
+++ HotelManagement/src/test/java/com/deltasource/hotelmanagement/test/ManagerTest.java
@@ -1,12 +1,18 @@
 package com.deltasource.hotelmanagement.test;
 
+import com.deltasource.hotelmanagement.core.Hotel;
 import com.deltasource.hotelmanagement.core.Manager;
+import com.deltasource.hotelmanagement.core.Room;
+import com.deltasource.hotelmanagement.core.commodities.Bed;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ManagerTest {
-
 	@Test
 	public void setNameShouldThrowExceptionWhenNameIsNull() {
 		// then
@@ -26,5 +32,24 @@ public class ManagerTest {
 			// when
 			manager.setHotel(null);
 		});
+	}
+
+	@Test
+	public void roomShouldBeBookedAfterManagerBooksIt() {
+		// given
+		Room room = new Room(1);
+		room.getCommodities().add(new Bed(1, 1));
+		Hotel hotel = new Hotel("Hotel");
+		hotel.getRooms().add(room);
+		Manager manager = new Manager("Manager");
+		manager.setHotel(hotel);
+		LocalDate fromDate = LocalDate.of(2019, 7, 16);
+		LocalDate toDate = LocalDate.of(2019, 7, 20);
+
+		// when
+		manager.bookRoom(1, fromDate, toDate, 4, 1, "guest name", "001");
+
+		// then
+		assertThat(room.isBooked(fromDate, toDate, 4), equalTo(true));
 	}
 }
